097bf9893d84485acd7c7db9f2219d41fff637d1
d0tr4ck3t33r <stein322@gmail.com>
2017-01-17 08:36:51 +0100

    Tests for the existing API finished

--- chat.py
+++ chat.py
@@ -47,12 +47,19 @@ class chat_client(object):
     #General Methods e.g. List all Users
 
     @keep_client_alive
-    def listAllUsers(self):
+    def listAllUsersRaw(self):
         keys = r.keys('user_*')
         if DEBUG:
             print("~KEYS user_*")
             print("~~"+str(keys))
         return keys
+
+    def listAllUsers(self):
+        raw_user_list = self.listAllUsersRaw()
+        user_list = []
+        for u in raw_user_list:
+            user_list += [u[5:]]
+        return user_list
 
     #Methods Message Releated
     @keep_client_alive
@@ -73,24 +80,49 @@ class chat_client(object):
             print("~DEL " + key)
         return messages
         
+    def getMessagesFromAll(self):
+        user_list = self.listAllUsers()
+        message_list = []
+        for u in user_list:
+            mes = self.getMessagesFrom(u)
+            for m in mes:
+                message_list += [(u, m)]
+        return message_list
         
+
 
 def main():
     username1 = "gunter"
     username2 = "peter"
-    user1 = chat_client("gunter")
-    print(user1.checkIfAlive())
-    user2 = chat_client("peter")
-    print(user2.checkIfAlive())
+    username3 = "petra"
+
+    user1 = chat_client(username1)
+    user2 = chat_client(username2)
+    user3 = chat_client(username3)
+
+    assert(user1.checkIfAlive())
+    
+    assert(username1 in user1.listAllUsers())
+    assert(username2 in user1.listAllUsers())
+    assert(username3 in user3.listAllUsers())
 
 
-    print(user1.listAllUsers())
-    message1 = "Hello " + username1
-    print("Message 1: " + message1)
-    user2.sendMessageTo(username1, message1)
-    print("Got: " + ';'.join(user1.getMessagesFrom(username2)))
+    _1to2mes = "Ya whats up"
+    _1to3mes = "Ayy lmao"
+    _1to1mes = "It's me Mario"
+    _2to3mes = "Yeee"
 
+    user1.sendMessageTo(username2, _1to2mes)
+    user1.sendMessageTo(username3, _1to3mes)
+    user1.sendMessageTo(username1, _1to1mes)
+    user2.sendMessageTo(username3, _2to3mes)
 
+    user1_messages = user1.getMessagesFromAll()
+    assert(user1_messages == [(username1, _1to1mes)])
+    user2_messages = user2.getMessagesFrom(username1)
+    assert(user2_messages == [_1to2mes])
+    user3_messages = user3.getMessagesFromAll()
+    assert(user3_messages == [(username1, _1to3mes), (username2, _2to3mes)])
 
 
 if __name__ == "__main__":
