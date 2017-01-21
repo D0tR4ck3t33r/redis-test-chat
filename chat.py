9cc69e6558b465fd9b9e62abeb9f3d23b1f18162
d0tr4ck3t33r <stein322@gmail.com>
2017-01-21 21:02:54 +0100

    Started with the parsing of messages. Autocompletion is working

--- chat.py
+++ chat.py
@@ -1,30 +1,46 @@
 import readline
+from client import chat_client
 
+chat_commands = ["list", "send"]
 
 
 
 
 class CommandCompleter(object):
+   
     def __init__(self, options):
         self.options = sorted(options)
-        self.matches = []
+        self.matches = None
 
     def complete(self, text, state):
-        if state == 0:
-            if text:
-                self.matches = [s for s in self.options if s and
-                        s.startwith(text)]
-            else:
+        if state == 0:  # on first trigger, build possible matches
+            if text:  # cache matches (entries that start with entered text)
+                self.matches = [s for s in self.options 
+                                    if s and s.startswith(text)]
+            else:  # no text entered, all matches possible
                 self.matches = self.options[:]
-        try:
+
+        # return match indexed by state
+        try: 
             return self.matches[state]
         except IndexError:
             return None
 
+def parse(line):
+    line = line.strip()
+    return line.split()
+
+
+
+
 #TODO: Gui
 def main():
+    #Get login
+    username = raw_input("username: ")
+    user = chat_client(username)
+    allusers = user.listAllUsers()
+
     #Set the commpleter for command completion inside our chat
-    chat_commands = ["list", "send"]
     completer = CommandCompleter(chat_commands)
     readline.set_completer(completer.complete)
     readline.parse_and_bind('tab: complete')
@@ -32,7 +48,9 @@ def main():
         readline.add_history(kw)
 
     #Input processing starts here
-    
+    while True:
+        line = raw_input('>')
+        print(parse(line))
     
 
 
