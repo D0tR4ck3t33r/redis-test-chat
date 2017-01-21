1d81b9b88e001513a980888e5c99c20f5e660be0
d0tr4ck3t33r <stein322@gmail.com>
2017-01-21 19:13:22 +0100

    Removed curses dependency and is now using just libreadline

--- chat.py
+++ chat.py
@@ -1,51 +1,41 @@
-import curses
-from client import *
+import readline
 
-       
+
+
+
+
+class CommandCompleter(object):
+    def __init__(self, options):
+        self.options = sorted(options)
+        self.matches = []
+
+    def complete(self, text, state):
+        if state == 0:
+            if text:
+                self.matches = [s for s in self.options if s and
+                        s.startwith(text)]
+            else:
+                self.matches = self.options[:]
+        try:
+            return self.matches[state]
+        except IndexError:
+            return None
+
 #TODO: Gui
 def main():
-    myscreen = curses.initscr()
-    myscreen.border(1)
-    myscreen.addstr(12, 25, "yee is alive")
-    myscreen.refresh()
-    myscreen.getch()
-    curses.endwin()
+    #Set the commpleter for command completion inside our chat
+    chat_commands = ["list", "send"]
+    completer = CommandCompleter(chat_commands)
+    readline.set_completer(completer.complete)
+    readline.parse_and_bind('tab: complete')
+    for kw in chat_commands:
+        readline.add_history(kw)
 
-
-
-def tests():
-    username1 = "gunter"
-    username2 = "peter"
-    username3 = "petra"
-
-    user1 = chat_client(username1)
-    user2 = chat_client(username2)
-    user3 = chat_client(username3)
-
-    assert(user1.checkIfAlive())
+    #Input processing starts here
     
-    assert(username1 in user1.listAllUsers())
-    assert(username2 in user1.listAllUsers())
-    assert(username3 in user3.listAllUsers())
+    
 
-
-    _1to2mes = "Ya whats up"
-    _1to3mes = "Ayy lmao"
-    _1to1mes = "It's me Mario"
-    _2to3mes = "Yeee"
-
-    user1.sendMessageTo(username2, _1to2mes)
-    user1.sendMessageTo(username3, _1to3mes)
-    user1.sendMessageTo(username1, _1to1mes)
-    user2.sendMessageTo(username3, _2to3mes)
-
-    user1_messages = user1.getMessagesFromAll()
-    assert(user1_messages == [(username1, _1to1mes)])
-    user2_messages = user2.getMessagesFrom(username1)
-    assert(user2_messages == [_1to2mes])
-    user3_messages = user3.getMessagesFromAll()
-    assert(user3_messages == [(username1, _1to3mes), (username2, _2to3mes)])
 
 
 if __name__ == "__main__":
-    tests()
+    main()
