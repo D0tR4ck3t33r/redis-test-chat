de153aabe222a698aa0bb6199a9a7d493ff2a9cf
d0tr4ck3t33r <stein322@gmail.com>
2017-01-30 06:51:04 +0100

    Gui

--- chat.py
+++ chat.py
@@ -1,7 +1,8 @@
 import readline
+import sys
 from client import chat_client
 
-chat_commands = ["list", "send"]
+chat_commands = ["help", "list", "send", "receive", "exit"]
 
 
 
@@ -27,18 +28,54 @@ class CommandCompleter(object):
             return None
 
 def parse(line):
-    line = line.strip()
-    return line.split()
+    #Parses the different commands into lists always starting with the command
+    #name. Lists can have different lenghts
+    l = line.strip()
+    if l.startswith("send"):
+        if l.split() == False:
+            return None
+        return l.split(' ', 2)
+    elif l.startswith("list")or l.startswith("help")or l.startswith("receive"):
+        return l.split()[:1]
+    elif l.startswith("exit"):
+        return l.split()[:1]
+    else:
+        return None
+
+def run_command(parsed_line, user):
+    #Used for the different commands to handle
+    command = parsed_line[0]
+    if command == 'help':
+        print("Help for the simple Chat using redis")
+        print("list - Lists all users")
+        print("send <user> <message> - Send a message to a user")
+        print("receive - Receive all messages send to you")
+    elif command == 'list':
+        print("Users online: ")
+        allonline = user.listAllUsers()
+        print(' '.join(allonline))
+    elif command == 'send': 
+        try:
+            receiver = parsed_line[1]
+            message = parsed_line[2]
+        except IndexError:
+            print('''Invalid Syntax. Check the help for more information on this
+            command''')
+        user.sendMessageTo(receiver, message)
+    elif command == 'receive':
+        messages = user.getMessagesFromAll()
+        for m in messages:
+            print("From: " + m[0])
+            print("Message: " + m[1])
+    elif command == "exit":
+        sys.exit(0)
 
 
 
-
-#TODO: Gui
 def main():
     #Get login
     username = raw_input("username: ")
     user = chat_client(username)
-    allusers = user.listAllUsers()
 
     #Set the commpleter for command completion inside our chat
     completer = CommandCompleter(chat_commands)
@@ -50,7 +87,12 @@ def main():
     #Input processing starts here
     while True:
         line = raw_input('>')
-        print(parse(line))
+        parsed_line = parse(line)
+        if parsed_line is None:
+            print("Error: No command with that name found")
+            print("Check help for available commands")
+            continue
+        run_command(parsed_line, user)
     
 
 
